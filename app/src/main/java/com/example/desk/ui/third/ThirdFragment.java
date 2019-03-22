a4d031edd0ed0363cb7893124784ca18575686c9
13608428279@163.com <13608428279@163.com>
2019-03-22 17:45:29 +0800

    2019/3/22 17：55

--- app/src/main/java/com/example/desk/ui/third/ThirdFragment.java
+++ app/src/main/java/com/example/desk/ui/third/ThirdFragment.java
@@ -1,35 +1,64 @@
 package com.example.desk.ui.third;
 
 
+import android.Manifest;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.desk.R;
 import com.example.desk.entity.MyState;
 import com.example.desk.mvp.MVPBaseFragment;
+import com.example.desk.ui.myinfo.MyinfoActivity;
+import com.example.desk.util.TLog;
 import com.leon.lib.settingview.LSettingItem;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
 import de.hdodenhof.circleimageview.CircleImageView;
+import pub.devrel.easypermissions.AppSettingsDialog;
+import pub.devrel.easypermissions.EasyPermissions;
+
+import static android.support.v4.media.MediaBrowserServiceCompat.RESULT_OK;
 
 /**
  * MVPPlugin
  * 邮箱 784787081@qq.com
  */
 
-public class ThirdFragment extends MVPBaseFragment<ThirdContract.View, ThirdPresenter> implements ThirdContract.View {
+public class ThirdFragment extends MVPBaseFragment<ThirdContract.View, ThirdPresenter> implements ThirdContract.View, View.OnClickListener ,EasyPermissions.PermissionCallbacks{
 
     @BindView(R.id.profile_image)
     CircleImageView profileImage;
@@ -49,6 +78,20 @@ public class ThirdFragment extends MVPBaseFragment<ThirdContract.View, ThirdPres
 
     private AlertDialog alertDialog;
     private ProgressDialog progressDialog;
+    private Dialog dialog;
+    //相册请求码
+    private static final int ALBUM_REQUEST_CODE = 1;
+    //相机请求码
+    private static final int CAMERA_REQUEST_CODE = 2;
+    //剪裁请求码
+    private static final int CROP_SMALL_PICTURE = 3;
+
+    //调用照相机返回图片文件
+    private File tempFile;
+    //最后显示的图片文件
+    private  String mFile;
+
+
     public ThirdFragment() {
     }
 
@@ -78,8 +121,24 @@ public class ThirdFragment extends MVPBaseFragment<ThirdContract.View, ThirdPres
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.profile_image:
+                dialog = new Dialog(getActivity(),R.style.ActionSheetDialogStyle);
+                View inflate = LayoutInflater.from(getActivity()).inflate(R.layout.touxiang, null);
+                Button choosePhoto = (Button) inflate.findViewById(R.id.choosePhoto);
+                Button takePhoto = (Button) inflate.findViewById(R.id.takePhoto);
+                Button cancel = (Button) inflate.findViewById(R.id.btn_cancel);
+                choosePhoto.setOnClickListener(this);
+                takePhoto.setOnClickListener(this);
+                cancel.setOnClickListener(this);
+                dialog.setContentView(inflate);
+                Window dialogWindow = dialog.getWindow();
+                dialogWindow.setGravity( Gravity.BOTTOM);
+                WindowManager.LayoutParams lp = dialogWindow.getAttributes();
+                lp.y = 20;
+                dialogWindow.setAttributes(lp);
+                dialog.show();
                 break;
             case R.id.item_one_myinfo:
+                startActivity(new Intent(getActivity(),MyinfoActivity.class));
                 break;
             case R.id.item_two_myinfo:
                 //当前状态
@@ -140,6 +199,17 @@ public class ThirdFragment extends MVPBaseFragment<ThirdContract.View, ThirdPres
                 alertDialog.show();
                 break;
             case R.id.item_five_myinfo:
+                AlertDialog.Builder builder2 = new AlertDialog.Builder(getActivity());
+                builder2.setTitle("软件说明");
+                builder2.setMessage("***************************************************************************************************************");
+                builder2.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        alertDialog.dismiss();
+                    }
+                });
+                alertDialog = builder2.create();
+                alertDialog.show();
                 break;
         }
     }
@@ -169,5 +239,171 @@ public class ThirdFragment extends MVPBaseFragment<ThirdContract.View, ThirdPres
         });
         alertDialog = builder.create();
         alertDialog.show();
+    }
+
+    /**
+     * 重写onRequestPermissionsResult，用于接受请求结果
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //将请求结果传递EasyPermission库处理
+        TLog.error("onRequestPermissionsResult:"+ requestCode);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.takePhoto:
+                if (EasyPermissions.hasPermissions(getActivity(),Manifest.permission.CAMERA)){
+                    getPicFromCamera();//拍照
+                }else {
+                    // request for one permission
+                    EasyPermissions.requestPermissions(this, getString(R.string.rationale_camera),
+                            CAMERA_REQUEST_CODE, Manifest.permission.CAMERA);
+                }
+
+                break;
+            case R.id.choosePhoto:
+                getPicFromAlbm();//从系统相册选择
+                break;
+            case R.id.btn_cancel:
+                dialog.dismiss();
+        }
+    }
+
+    private void getPicFromCamera() {
+        //用于保存调用相机拍照后所生成的文件
+        tempFile = new File(Environment.getExternalStorageDirectory().getPath(), System.currentTimeMillis() + ".png");
+        //跳转到调用系统相机
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //判断版本
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {   //如果在Android7.0以上,使用FileProvider获取Uri
+            intent.setFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            Uri contentUri = FileProvider.getUriForFile(getActivity(), getActivity().getPackageName()+".provider", tempFile);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, contentUri);
+            Log.e("getPicFromCamera", contentUri.toString());
+        } else {    //否则使用Uri.fromFile(file)方法获取Uri
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(tempFile));
+        }
+        startActivityForResult(intent, CAMERA_REQUEST_CODE);
+    }
+    /**
+     * 从相册获取图片
+     */
+    private void getPicFromAlbm() {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, ALBUM_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        switch (requestCode) {
+            case CAMERA_REQUEST_CODE:   //调用相机后返回
+                if (resultCode == RESULT_OK) {
+                    //用相机返回的照片去调用剪裁也需要对Uri进行处理
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        Uri contentUri = FileProvider.getUriForFile(getActivity(), getActivity().getPackageName()+".provider", tempFile);
+                        startPhotoZoom(contentUri);//开始对图片进行裁剪处理
+                    } else {
+                        startPhotoZoom(Uri.fromFile(tempFile));//开始对图片进行裁剪处理
+                    }
+                }
+                break;
+            case ALBUM_REQUEST_CODE:    //调用相册后返回
+                if (resultCode == RESULT_OK) {
+                    Uri uri = intent.getData();
+                    startPhotoZoom(uri); // 开始对图片进行裁剪处理
+                }
+                break;
+            case CROP_SMALL_PICTURE:  //调用剪裁后返回
+                if (intent != null) {
+                    // 让刚才选择裁剪得到的图片显示在界面上
+                    //TODO:得到的mFile可能为空，拿到后需要传给服务端
+                    Bitmap photo =BitmapFactory.decodeFile(mFile);
+                    profileImage.setImageBitmap(photo);
+                } else {
+                    Log.e("data","data为空");
+                }
+                break;
+        }
+
+    }
+
+    private void startPhotoZoom(Uri uri) {
+        if (uri == null) {
+            Log.i("tag", "The uri is not exist.");
+        }
+//        tempUri = uri;
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        intent.setDataAndType(uri, "image/*");
+        // 设置裁剪
+        intent.putExtra("crop", "true");
+        // aspectX aspectY 是宽高的比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        // outputX outputY 是裁剪图片宽高
+        intent.putExtra("outputX", 100);
+        intent.putExtra("outputY", 100);
+        intent.putExtra("return-data", false);
+        File out = new File(getPath());
+        if (!out.getParentFile().exists()) {
+            out.getParentFile().mkdirs();
+        }
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(out));
+        startActivityForResult(intent, CROP_SMALL_PICTURE);
+    }
+    //裁剪后的地址
+    public  String getPath() {
+        //resize image to thumb
+        if (mFile == null) {
+            mFile = Environment.getExternalStorageDirectory() + "/" +"wode/"+ "outtemp.png";
+        }
+        return mFile;
+    }
+    public String saveImage(String name, Bitmap bmp) {
+        File appDir = new File(Environment.getExternalStorageDirectory().getPath());
+        if (!appDir.exists()) {
+            appDir.mkdir();
+        }
+        String fileName = name + ".png";
+        File file = new File(appDir, fileName);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.flush();
+            fos.close();
+            return file.getAbsolutePath();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+        getPicFromCamera();//拍照
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+        Toast.makeText(getActivity(),"用户授权失败",Toast.LENGTH_SHORT).show();
+        /**
+         * 若是在权限弹窗中，用户勾选了'NEVER ASK AGAIN.'或者'不在提示'，且拒绝权限。
+         * 这时候，需要跳转到设置界面去，让用户手动开启。
+         */
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            new AppSettingsDialog.Builder(this).build().show();
+        }
     }
 }
